1568b9da89492b98a28e653d725e410f6411e937
Hudson Van-Dal <hudson.vandal@bairesdev.com>
2022-07-06 06:27:40 -0300

    feat admin overview component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
+import { OverviewComponent } from './admin/components/overview/overview.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent
   ],
   imports: [
     BrowserModule,
